c50ce02a136beb7ad4ee0d0be7f4e03b0e7cfaf7
Varsha Srivastava <srivastavavarsha9506@gmail.com>
2026-09-06 09:08:35 +0530

    Updated rudram  website

--- server/config/database.js
+++ server/config/database.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'rudram_db',
   process.env.DB_USER || 'root',
+  
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false, // Set to true if you want to see SQL queries in console
   }
--- server/server.js
+++ server/server.js
@@ -52,6 +52,7 @@ const init = async () => {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
+      port: process.env.DB_PORT || 3306,
       password: process.env.DB_PASSWORD || '',
     });
     
